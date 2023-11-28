fbb5bce50e492c80ed24a4c849c969bfdc9a5294
victoryforphil <victoryforphil@users.noreply.github.com>
2023-11-28 20:16:50 +0000

    deploy: 40a83865d109e2569ff569ef17fdf83f64a615be

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["deranged","game_maths","itoa","libc","log","num_threads","powerfmt","simplelog","termcolor","time","time_core","time_macros"];
---
+++ src-files.js
@@ -0,0 +1,15 @@
+var srcIndex = JSON.parse('{\
+"deranged":["",[],["lib.rs","traits.rs"]],\
+"game_maths":["",[["maths",[],["mod.rs"]]],["lib.rs"]],\
+"itoa":["",[],["lib.rs","udiv128.rs"]],\
+"libc":["",[["unix",[["linux_like",[["linux",[["arch",[["generic",[],["mod.rs"]]],["mod.rs"]],["gnu",[["b64",[["x86_64",[],["align.rs","mod.rs","not_x32.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["align.rs","mod.rs","non_exhaustive.rs"]]],["mod.rs"]]],["align.rs","mod.rs"]]],["fixed_width_ints.rs","lib.rs","macros.rs"]],\
+"log":["",[],["__private_api.rs","lib.rs","macros.rs"]],\
+"num_threads":["",[],["lib.rs","linux.rs"]],\
+"powerfmt":["",[],["buf.rs","ext.rs","lib.rs","smart_display.rs","smart_display_impls.rs"]],\
+"simplelog":["",[["loggers",[],["comblog.rs","logging.rs","mod.rs","simplelog.rs","termlog.rs","writelog.rs"]]],["config.rs","lib.rs"]],\
+"termcolor":["",[],["lib.rs"]],\
+"time":["",[["error",[],["component_range.rs","conversion_range.rs","different_variant.rs","format.rs","indeterminate_offset.rs","invalid_format_description.rs","invalid_variant.rs","mod.rs"]],["format_description",[["parse",[],["ast.rs","format_item.rs","lexer.rs","mod.rs"]],["well_known",[["iso8601",[],["adt_hack.rs"]]],["iso8601.rs","rfc2822.rs","rfc3339.rs"]]],["borrowed_format_item.rs","component.rs","mod.rs","modifier.rs","owned_format_item.rs"]],["formatting",[],["formattable.rs","iso8601.rs","mod.rs"]],["sys",[["local_offset_at",[],["mod.rs","unix.rs"]]],["mod.rs"]]],["date.rs","date_time.rs","duration.rs","ext.rs","instant.rs","internal_macros.rs","lib.rs","macros.rs","month.rs","offset_date_time.rs","primitive_date_time.rs","time.rs","utc_offset.rs","util.rs","weekday.rs"]],\
+"time_core":["",[],["convert.rs","lib.rs","util.rs"]],\
+"time_macros":["",[["format_description",[["public",[],["component.rs","mod.rs","modifier.rs"]]],["ast.rs","format_item.rs","lexer.rs","mod.rs"]],["helpers",[],["mod.rs","string.rs"]]],["date.rs","datetime.rs","error.rs","lib.rs","offset.rs","quote.rs","time.rs","to_tokens.rs"]]\
+}');
+createSrcSidebar();
