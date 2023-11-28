1a58462918b9c5741c471d08bdb2253956eaf8aa
Alex Carter <victoryforphil@gmail.com>
2023-11-28 13:22:43 -0800

    Scalar Operations + array converisons

--- src/maths/vector_3.rs
+++ src/maths/vector_3.rs
@@ -1,6 +1,6 @@
 use std::{
     fmt::Debug,
-    ops::{Index, IndexMut},
+    ops::{Index, IndexMut, Mul, DivAssign, MulAssign, Div},
 };
 
 #[derive(Clone, Copy)]
@@ -43,6 +43,42 @@ impl Index<usize> for Vector3D {
     }
 }
 
+/// Convert from Vector3D to array
+/// 
+/// # Example
+/// ```
+/// use game_maths::maths::Vector3D;
+/// 
+/// let vector_3d = Vector3D::new(1.0, 2.0, 3.0);
+/// let array: [f64; 3] = vector_3d.into();
+/// assert_eq!(array, [1.0, 2.0, 3.0]);
+impl From<Vector3D> for [f64; 3] {
+    fn from(vector_3d: Vector3D) -> Self {
+        [vector_3d.x, vector_3d.y, vector_3d.z]
+    }
+}
+
+impl From<[f64; 3]> for Vector3D {
+    fn from(array: [f64; 3]) -> Self {
+        Self {
+            x: array[0],
+            y: array[1],
+            z: array[2],
+        }
+    }
+}
+impl From<Vector3D> for [f32; 3] {
+    fn from(vector_3d: Vector3D) -> Self {
+        [vector_3d.x as f32, vector_3d.y as f32, vector_3d.z as f32]
+    }
+}
+
+impl PartialEq for Vector3D {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y && self.z == self.z
+    } 
+}
+
 /// IndexMut impl for Vector3D
 ///
 /// # Example
@@ -50,12 +86,10 @@ impl Index<usize> for Vector3D {
 ///     use game_maths::maths::Vector3D;
 ///
 ///    let mut vector_3d = Vector3D::new(1.0, 2.0, 3.0);
-///   vector_3d[0] = 4.0;
-///  vector_3d[1] = 5.0;
-/// vector_3d[2] = 6.0;
-/// assert_eq!(vector_3d[0], 4.0);car
-/// assert_eq!(vector_3d[1], 5.0);
-/// assert_eq!(vector_3d[2], 6.0);
+///     vector_3d[0] = 4.0;
+///     vector_3d[1] = 5.0;
+///     vector_3d[2] = 6.0;
+///     assert_eq!(vector_3d, [4.0, 5.0, 6.0].into());
 /// ```
 impl IndexMut<usize> for Vector3D {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
@@ -68,6 +102,66 @@ impl IndexMut<usize> for Vector3D {
     }
 }
 
+/// Scalar multiplication for Vector3D
+/// 
+/// # Example
+/// ```
+/// use game_maths::maths::Vector3D;
+/// 
+/// let vector_3d = Vector3D::new(1.0, 2.0, 3.0);
+/// let vector_3d = vector_3d * 2.0;
+/// assert_eq!(vector_3d, [2.0, 4.0, 6.0].into());
+/// ```
+impl Mul<f64> for Vector3D {
+    type Output = Self;
+    fn mul(self, s: f64) -> Self::Output {
+        // S = scalar
+        Self {
+            x: self.x * s,
+            y: self.y * s,
+            z: self.z * s,
+        }
+    }
+}
+
+impl MulAssign<f64> for Vector3D {
+    fn mul_assign(&mut self, s: f64) {
+        // S = scalar
+        self.x *= s;
+        self.y *= s;
+        self.z *= s;
+    }
+}
+/// Scalar division for Vector3D
+/// 
+/// # Example
+/// ```
+/// use game_maths::maths::Vector3D;
+/// 
+/// let vector_3d = Vector3D::new(1.0, 2.0, 3.0);
+/// let vector_3d = vector_3d / 2.0;
+/// assert_eq!(vector_3d, [0.5, 1.0, 1.5].into());
+
+impl Div<f64> for Vector3D {
+    type Output = Self;
+    fn div(self, s: f64) -> Self::Output {
+        // S = scalar
+        Self {
+            x: self.x / s,
+            y: self.y / s,
+            z: self.z / s,
+        }
+    }
+}
+
+impl DivAssign<f64> for Vector3D {
+    fn div_assign(&mut self, s: f64) {
+        // S = scalar
+        self.x /= s;
+        self.y /= s;
+        self.z /= s;
+    }
+}
 impl Vector3D {
     /// Creates a new Vector3D
     ///
@@ -81,9 +175,7 @@ impl Vector3D {
     /// use game_maths::maths::Vector3D;
     ///
     /// let vector_3d = Vector3D::new(1.0, 2.0, 3.0);
-    /// assert_eq!(vector_3d.x, 1.0);
-    /// assert_eq!(vector_3d.y, 2.0);
-    /// assert_eq!(vector_3d.z, 3.0);
+    /// assert_eq!(vector_3d, [1.0, 2.0, 3.0].into());
     /// ```
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
@@ -99,9 +191,7 @@ impl Vector3D {
     /// use game_maths::maths::Vector3D;
     ///
     /// let vector_3d = Vector3D::from_slice(&[1.0, 2.0, 3.0]);
-    /// assert_eq!(vector_3d.x, 1.0);
-    /// assert_eq!(vector_3d.y, 2.0);
-    /// assert_eq!(vector_3d.z, 3.0);
+    /// assert_eq!(vector_3d, [1.0, 2.0, 3.0].into());
     /// ```
     pub fn from_slice(slice: &[f64]) -> Self {
         Self {
@@ -121,9 +211,7 @@ impl Vector3D {
     /// use game_maths::maths::Vector3D;
     ///
     /// let vector_3d = Vector3D::from_array([1.0, 2.0, 3.0]);
-    /// assert_eq!(vector_3d.x, 1.0);
-    /// assert_eq!(vector_3d.y, 2.0);
-    /// assert_eq!(vector_3d.z, 3.0);
+    /// assert_eq!(vector_3d, [1.0, 2.0, 3.0].into());
     /// ```
     pub fn from_array(array: [f64; 3]) -> Self {
         Self {
@@ -179,5 +267,29 @@ mod tests {
         assert_eq!(vector_3d[0], 4.0);
         assert_eq!(vector_3d[1], 5.0);
         assert_eq!(vector_3d[2], 6.0);
+    }
+
+    #[test]
+    fn test_vector_3d_into() {
+        let vector_3d = Vector3D::new(1.0, 2.0, 3.0);
+        let array: [f64; 3] = vector_3d.into();
+        assert_eq!(array, [1.0, 2.0, 3.0]);
+    }
+
+    #[test]
+    fn test_vector_3d_from() {
+        let vector_3d = Vector3D::from([1.0, 2.0, 3.0]);
+        assert_eq!(vector_3d, [1.0, 2.0, 3.0].into());
+    }
+
+    #[test]
+    fn test_vector_3d_scalar() {
+        let vector_3d = Vector3D::new(1.0, 2.0, 3.0);
+        let vector_3d = vector_3d * 2.0;
+        assert_eq!(vector_3d, [2.0, 4.0, 6.0].into());
+
+        let vector_3d = Vector3D::new(1.0, 2.0, 3.0);
+        let vector_3d = vector_3d / 2.0;
+        assert_eq!(vector_3d, [0.5, 1.0, 1.5].into());
     }
 }
